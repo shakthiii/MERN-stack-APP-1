a0abe485e1d610dd5ab1a8e94fedd64660d00166
Sakthivel M <sakthivel@innoventes.co>
2022-11-15 14:24:19 +0530

    Change the PORT number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.get("/", (req, res) => {
   res.send("API running");
